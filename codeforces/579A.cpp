f771cea1f37fcbe14056c53d314e1a24dc761b96
michaelayad <micheltwfik@gmail.com>
2018-03-11 22:09:05 +0200

    mnmn

---
+++ codeforces/579A.cpp
@@ -0,0 +1,35 @@
+ #define _CRT_SECURE_NO_WARNINGS
+//             »”„ «·À«·ÊÀ «·«ﬁœ”
+
+#include<iostream>	
+#include<algorithm>
+#include<cstdio>
+#include<sstream>
+#include<cmath>
+#include<climits>
+#include<string>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<map>
+#include<queue>
+#include<deque>
+#include<set>
+#include<fstream>
+#include<bitset>
+using namespace std;
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+
+
+	//freopen("keys.in", "r", stdin);
+	int x;
+	cin >> x;
+	bitset<32>a1(x);
+	cout << a1.count() << endl;
+
+	system("pause");
+	return 0;
+}
