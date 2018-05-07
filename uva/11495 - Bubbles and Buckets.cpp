9f06fd678bf62e925d9bd1186238af5e77a7c7af
michaelayad <31035708+michaelayad@users.noreply.github.com>
2018-05-07 16:07:29 +0300

    ya raby

---
+++ uva/11495 - Bubbles and Buckets.cpp
@@ -0,0 +1,89 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//             »”„ «·À«·ÊÀ «·«ﬁœ”
+
+#include<iostream>	
+#include<algorithm>
+#include<cstdio>
+#include<sstream>
+#include<cmath>
+#include<climits>
+#include<string>
+#include<iomanip>
+#include<vector>
+#include<stack>
+#include<map>
+#include<queue>
+#include<deque>
+#include<set>
+#include<fstream>
+#include<bitset>
+#include <cstring>
+using namespace std;
+
+//jesus help me.......
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+///////////////////////////////////////////////////////////////		
+
+
+int co[1000000], arr[200005];
+void Merge_Sort(int arr[], int l, int r,long long &inv)
+{
+	if (r - l <= 1)
+		return;
+	int md = (l + r) / 2;
+	Merge_Sort(arr, l, md,inv);
+	Merge_Sort(arr, md, r,inv);
+	int x = l, y = md, ind = 0;
+	while (x < md&&y < r)
+	{
+		if (arr[x] <= arr[y])
+			co[ind++] = arr[x], x++;
+		else
+		{
+			co[ind++] = arr[y], y++;
+			inv += (md - x);
+		}
+	}
+	while (x < md)
+	{
+		co[ind++] = arr[x]; x++;
+
+	}
+	while (y < r)
+	{
+		co[ind++] = arr[y]; y++;
+
+	}
+	ind = 0;
+	for (int i = l;i < r;i++)
+	{
+		arr[i] = co[ind++];
+	}
+
+}
+///////////////////////////////////////////////////////////
+int main()
+{
+	fast();
+	//freopen("keys.in", "r", stdin);
+	int n;
+	while (cin >> n&&n)
+	{
+		long long inv = 0;	
+		for (int i = 0;i < n;i++)
+		{
+			cin >> arr[i];
+		}
+		Merge_Sort(arr, 0, n, inv);
+		if (inv % 2 == 1)
+			cout << "Marcelo" << endl;
+		else
+			cout << "Carlos" << endl;
+	}
+	//system("pause");
+	return 0;
+}
